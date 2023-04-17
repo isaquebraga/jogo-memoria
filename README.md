c3e991110052e9a65f2709e5519492872f8910b8
isaque <isaquesouzabraga@gmail.com>
2023-04-17 18:50:24 -0300

    finaliza jogo

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# Jogo da Memória dos Heróis da História Brasileira
+
+Este é um jogo da memória simples baseado em personagens históricos do Brasil. O objetivo é encontrar todos os pares de cartas correspondentes. 
+
+Vale ressaltar que não são pares comuns. Eles são compostos por uma foto do personagem e por uma descrição.
+
+
+## Autores
+
+- [@isaquebraga](https://github.com/isaquebraga)
+- [@alexiarodrigues](https://github.com/alexias1)
+- Maria Graziella
+
+
+## Como jogar
+
+O projeto pode ser facilmente acessado através do link que se encontra na descrição do repositório.
+
+    1. Abra o jogo em um navegador web moderno com suporte a JavaScript.
+    2. Insira seu nome e clique no botão "Jogar" para começar.
+    3. Você não terá um tempo limitado para encontrar todos os pares de cartas correspondentes. No entanto, no final do jogo, será mostrado o tempo que você levou para encontrat todos os pares.
+    4. Clique em uma carta para virá-la e revelar a imagem.
+    5. Clique em outra carta para tentar encontrar o par correspondente.
+    6. Se você acertar, as duas cartas permanecerão viradas para cima. Caso contrário, elas serão viradas de volta para baixo.
+    7. Continue jogando até encontrar todos os pares de cartas correspondentes.
+
+
+
+## Tecnologias Utilizadas
+
+Este jogo foi construído utilizando HTML, CSS e JavaScript puro, sem a utilização de bibliotecas ou frameworks adicionais.
+## Contribuindo
+
+Se você deseja contribuir com o projeto, sinta-se à vontade para enviar um pull request com suas sugestões de melhorias ou adição de novos personagens históricos.
+## Licença
+
+Este projeto é licenciado sob a licença MIT. Você é livre para utilizar, copiar, modificar e distribuir o código como desejar. Veja o arquivo [LICENSE](https://github.com/isaquebraga/jogo-memoria/blob/main/LICENSE) para mais informações.
+
---
+++ js/game.js
@@ -0,0 +1,124 @@
+const grid = document.querySelector(".grid");
+const spanPlayer = document.querySelector(".player");
+const timer = document.querySelector(".timer");
+
+const characters = [
+    "anita",
+    "anita-descricao",
+    "chica",
+    "chica-descricao",
+    "chiquinha",
+    "chiquinha-descricao",
+    "getulio",
+    "getulio-descricao",
+    "machado",
+    "machado-descricao",
+    "marechal",
+    "marechal-descricao",
+    "pedro",
+    "pedro-descricao",
+    "quiteria",
+    "quiteria-descricao",
+    "tarsila",
+    "tarsila-descricao",
+    "zumbi",
+    "zumbi-descricao"
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag)
+    element.className = className;
+    return element;
+}
+
+let firstCard = "";
+let secondCard = "";
+
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll(".disabled-card")
+
+    if (disabledCards.length === 20) {
+        clearInterval(this.loop);
+        alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML} segundos.`);
+    }
+}
+
+const checkCards = () => {
+    const firstCharacter = firstCard.getAttribute("data-character");
+    const secondCharacter = secondCard.getAttribute("data-character");
+
+    if (firstCharacter === secondCharacter) {
+        firstCard.firstChild.classList.add("disabled-card");
+        secondCard.firstChild.classList.add("disabled-card");
+
+        firstCard = "";
+        secondCard = "";
+
+        checkEndGame();
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove("reveal-card");
+            secondCard.classList.remove("reveal-card");
+
+            firstCard = "";
+            secondCard = "";
+        }, 500)
+    }
+}
+
+const revealCard = ({ target }) => {
+
+    if (target.parentNode.className.includes("reveal-card")) {
+        return;
+    }
+
+    if (firstCard === "") {
+        target.parentNode.classList.add("reveal-card");
+        firstCard = target.parentNode;
+    } else if (secondCard === ""){
+        target.parentNode.classList.add("reveal-card");
+        secondCard = target.parentNode;
+
+        checkCards();
+    }
+}
+
+const createCard = (character) => {
+    const card = createElement("div", "card")
+    const front = createElement("div", "face front")
+    const back = createElement("div", "face back")
+
+    front.style.backgroundImage = `url("/img/${character}.png")`;
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    var nomeCharacter = character.split("-");
+
+    card.addEventListener("click", revealCard);
+    card.setAttribute("data-character", nomeCharacter[0])
+
+    return card;
+}
+
+const loadGame = () => {
+    const shuffledCharacters = characters.sort(() => Math.random() - 0.5);
+
+    shuffledCharacters.forEach((character) => {
+        const card = createCard(character);
+        grid.appendChild(card);
+    })
+}
+
+const startTimer = () => {
+    this.loop = setInterval(() => {
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime + 1;
+    }, 1000)
+}
+
+window.onload = () => {
+    spanPlayer.innerHTML = localStorage.getItem("player");
+    startTimer();
+    loadGame();
+}
